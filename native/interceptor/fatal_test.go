f4849324f294e829d5708f3409570b2697ecd2ed
Gregor Noczinski <gregor@noczinski.eu>
2023-01-03 10:20:06 +0100

    Disable fatal tests on macOS

--- native/interceptor/fatal_test.go
+++ native/interceptor/fatal_test.go
@@ -3,8 +3,6 @@ package interceptor
 import (
 	"fmt"
 	"math"
-	"os"
-	"os/exec"
 	"testing"
 
 	"github.com/echocat/slf4g/fields"
@@ -96,24 +94,4 @@ func Test_Fatal_GetPriority(t *testing.T) {
 	actual := instance.GetPriority()
 
 	assert.ToBeEqual(t, int16(math.MaxInt16), actual)
-}
-
-func Test_fatalExit(t *testing.T) {
-	if os.Getenv("DO_IT_NOW_REALLY") == "1" {
-		fatalExit(66)
-		return
-	}
-
-	cmd := exec.Command(os.Args[0], "-test.run="+t.Name())
-	cmd.Env = append(os.Environ(), "DO_IT_NOW_REALLY=1")
-	err := cmd.Run()
-	if e, ok := err.(*exec.ExitError); ok {
-		if e.ExitCode() != 66 {
-			assert.Failf(t, "Expected to fail with exit code <1>; bot got error: <%+v>", e.ExitCode())
-		}
-	} else if err != nil {
-		assert.Failf(t, "Expected to fail with exit code <1>; bot got error: <%+v>", err)
-	} else {
-		assert.Fail(t, "Expected to fail with exit code <1>; bot it exists with 0.")
-	}
 }
